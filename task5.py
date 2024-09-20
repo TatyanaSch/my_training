3b8b19323423e9d485adfbf187685e4b671ce17c
TatyanaSch <tatyana.s@sinorutrans.com>
2024-09-20 23:02:28 +0300

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,5 @@
+kurs_name= 'Python'
+count_tasks=12
+all_time=1.5
+time_for_1_task=(all_time/count_tasks)
+print ('Курс:'+kurs_name,', всего задач:',count_tasks,', затрачено часов:',all_time,',cреднее время выполнения:',time_for_1_task)
