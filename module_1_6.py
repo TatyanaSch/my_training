b81cc17af9c0aa30b1d07582de882bd953bd118d
TatyanaSch <tatyana.s@sinorutrans.com>
2024-09-21 23:44:39 +0300

    Add files via upload

--- module_1_6.py
+++ module_1_6.py
@@ -7,9 +7,9 @@ a= my_dict.pop ('Valera')
 print ('Deleted value:',a)
 print ('Modified dictionary:',my_dict)
 
-my_set = {5,'fly',3.14,5,3.14,True}
+my_set = {(4,5,3,6),'fly',5,True}
 print ('Set:',my_set)
 my_set.add(9)
-my_set.add(11)
+my_set.add(3.421)
 my_set.discard('fly')
 print ('Modified set:',my_set)
