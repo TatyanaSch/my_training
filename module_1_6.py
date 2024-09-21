cc27d3d275449f57f1e1f1251f7ba8b66aefbb04
TatyanaSch <tatyana.s@sinorutrans.com>
2024-09-21 23:33:07 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,15 @@
+my_dict = {'Tanya': 1976, 'Misha': 1974, 'Valera': 2003, 'Arseniy': 2013}
+print ('Dict:', my_dict)
+print ('Existing value:', my_dict ['Tanya'])
+print ('Not existing value:', my_dict.get ('Katya'))
+my_dict.update({'Lena': 2007, 'Sonya': 2010})
+a= my_dict.pop ('Valera')
+print ('Deleted value:',a)
+print ('Modified dictionary:',my_dict)
+
+my_set = {5,'fly',3.14,5,3.14,True}
+print ('Set:',my_set)
+my_set.add(9)
+my_set.add(11)
+my_set.discard('fly')
+print ('Modified set:',my_set)
