2e600dc1923aa6d1be10cdf23cc3565a8f0f5473
TatyanaSch <tatyana.s@sinorutrans.com>
2024-09-25 13:10:09 +0300

    Add files via upload

--- module_2_2.py
+++ module_2_2.py
@@ -3,7 +3,7 @@ second = int (input ('Введите второе число:'))
 third = int (input ('Введите третье число:'))
 if first==second and second==third:
     print (3)
-elif first==second and second!=third or  first==third and third!=second or second==third and third!= first:
+elif first==second or  first==third or second==third:
     print (2)
 else:
     print(0)
