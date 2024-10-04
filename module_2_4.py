4829abdec1d3e6fe84f91416159cf5475e136c28
TatyanaSch <tatyana.s@sinorutrans.com>
2024-10-04 15:22:26 +0300

    Add files via upload

--- module_2_4.py
+++ module_2_4.py
@@ -1,23 +1,18 @@
 #Домашняя работа по уроку "Цикл for. Элементы списка. Полезные функции в цикле"
-
+#Дано:
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes =[]
 not_primes = []
-is_prime = True
-for i in numbers [1:]:
-    for j in range (2,i):
-        if i % j == 0:
-            is_prime = False
+for i in numbers [1:]:# для каждого элемента в списке numbers,i число в списке, начинаем с элемента [1]
+    is_prime = True # простое число = истина
+    for j in numbers [1:int(i**0.5)+1]:# j- делитель, для делителя из списка numbers начиная с элемента [1] до числа равного квадратному корню элемента плюс еще одно число диапазона,т.е.включая последний элемент списка
+        if i % j == 0: # элемент в списке делится на делитель без остатка
+            is_prime = False # число  простое это ложь
             break
-        else:
-            is_prime = True
-            continue
-    if is_prime:
-        primes.append(i)
-    else:
-        not_primes.append(i)
-print ('Primes: ', primes)
-print ('Not Primes: ', not_primes)
+    primes.append(i) if is_prime else not_primes.append(i)  # добавляем в список primes если число простое иначе добавляем в список not_primes
+
+print ('Primes: ', primes) #выводим результат
+print ('Not Primes: ', not_primes) #выводим результат
 
 
 
