d6ad523cad3ce396678ad725c5e79f6002c4538c
TatyanaSch <tatyana.s@sinorutrans.com>
2024-11-12 23:32:16 +0300

    Add files via upload

--- module_3_1.py
+++ module_3_1.py
@@ -2,21 +2,26 @@ calls = 0
 def count_calls ():
     global calls
     calls += 1
-    def string_info (string):
-        count_calls ()
-        a = len (string)
-        b = string.upper ()
-        c = string.lower ()
-        return a,b,c
-        result1 = string_info("Привет Urban")
+def string_info (string):
+    count_calls ()
+    a = len (string)
+    b = string.upper ()
+    c = string.lower ()
+    return a,b,c
 
-        # def is_contains (string,list):
-        #     count_calls ()
-        #     d = string.lower ()
-        #     return d in (item.lower () for item in list)
-        result1 = string_info ("Привет Urban")
-        #     result2 = is_contains ("Mir", ["Привет","urban", "Знание"])
-        # print (result1)
-        # print (result2)
-    print (calls)
+def is_contains (string,list_1):
+    count_calls ()
+    d = string.lower ()
+    return d in (item.lower () for item in list_1)
 
+
+result1 = string_info("Привет пРЕПОДАватель")
+result2 = string_info("До свидания УЧЕник")
+result3 = is_contains ("знания", ["Привет","urban", "Знания"])
+result4 = is_contains ("Вид", ["Привет","urban", "Знание"])
+
+print (result1)
+print (result2)
+print (result3)
+print (result4)
+print (calls)
