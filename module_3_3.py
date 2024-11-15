36babad26b19d0f6e727c9dfcc6acda4e7571263
TatyanaSch <tatyana.s@sinorutrans.com>
2024-11-15 21:37:25 +0300

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,15 @@
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+
+
+print_params()
+print_params(b=25)
+print_params(c=[1, 2, 3])
+
+values_list = ['текст', False, 7]
+values_dict = {'a': 8, 'b': [3, 4], 'c': 3.6}
+print_params(*values_list)
+print_params(**values_dict)
+
+values_list_2 = [54.32, 'Строка']
+print_params(*values_list_2, 42)
